52610921bef4012257f87d4efe17bd0f892bbd85
harmik0919 <harmikrathod56@gmail.com>
2026-01-23 12:57:17 +0530

    23 jan

--- AWT/intercepting-route/app/api/student/[id]/route.tsx
+++ AWT/intercepting-route/app/api/student/[id]/route.tsx
@@ -1,7 +1,6 @@
 import { NextRequest } from "next/server"; 
 export async function GET(_req: NextRequest,{ params }: { params: { id: string 
 }}) { 
-// return "Hello world" 
 console.log(params) 
 return Response.json({name:'arjun',age:123}) 
 }
